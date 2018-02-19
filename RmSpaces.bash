c5d68ee581e36f987654e90452a4381cca687ee6
tursko <taylor@stykocompany.com>
2018-02-18 22:46:59 -0600

    Added catch for file extensions

--- RmSpaces.bash
+++ RmSpaces.bash
@@ -6,20 +6,41 @@
 #Example input: tursko     programming
 #Example output: tursko programming
 
-#Future additions: I want to be able to keep the same extention.
-#So if the input file is 'tursko.txt', currently it would make the
-#new output file 'tursko.txt_new'. I want it to be 'tursko_new.txt'
-
+#Added a catch for file extensions (2-18-2018)
+	#NOTE: if file name is something like FileName.s.txt , the resulting file name
+	#will be extremely off. 
+	#Only works for file names like: { file or file.txt or file.c or file.hey }
+echo "" > ExtNoName
+echo "" > ExtExists
 if [ $# -ne 1 ]; then
-    echo "Usage: "$0" [filename]"
-    exit 1;
+    	echo "Usage: "$0" [filename]"
+    	exit 1;
 fi
+
 if [ -e "$1" ]; then
-    #xt=sed 's/^\w\+.//' <<< "$1" #attempt at fixing extention issue
-    sed -E 's/  */ /g' "$1" > "$1_new"
-    echo "Successfully fixed all the spacing in the file '"$1"'"
-    echo "New file is called '$1_new'"
-    #echo "New file is called '"$1_New.$ext"'"
+	echo "$1" > fullName #this is just the filename
+	#grabbing file name and its ext
+	sed 's/\..*//g' fullName > FileNameNoExt
+	read FileName < FileNameNoExt
+	if grep -e "\." < fullName > ExtExists; then
+		sed 's/.*\.//g' fullName > ExtNoName
+		read FileExt < ExtNoName
+		
+		sed -E 's/  */ /g' "$1" > "$FileName-NEW.$FileExt"
+		echo "----------"
+		echo "Successfully removed all unnecessary spaces from $1"
+		echo "New file is called '$FileName-NEW.$FileExt'"
+	else
+		sed -E 's/  */ /g' "$1" > "$1-NEW"
+		echo "----------"
+		echo "Successfully removed all unnecessary spaces from $1"
+		echo "New file is called '$1-NEW'"
+	fi
 else
-    echo "The file '"$1"' does not exist"
+    	echo "The file '"$1"' does not exist"
 fi
+
+rm ExtNoName
+rm ExtExists
+rm fullName
+rm FileNameNoExt
